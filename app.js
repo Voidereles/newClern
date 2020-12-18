702f9b30c3fa3633157990e526c5c0832ef87dcc
Maciej <maciej.zelazo@protonmail.com>
2020-12-18 15:51:53 +0100

    animations

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import barba from '@barba/core';
-import gsap from "gsap";
+// import gsap from "gsap";
 
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel'
@@ -7,6 +7,30 @@ import 'owl.carousel'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.scss';
 import './js/pageTransitioning.js'
+
+// import anime from 'animejs/lib/anime.es.js';
+import anime from 'animejs/lib/anime.es.js';
+// import lax from 'lax.js'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// const anime = require('animejs');
+
+window.onload = function () {
+    AOS.init({
+        duration: 700,
+    });
+
+    // Add animation bindings to elements
+    // lax.addElements('.lax', {
+    //     scrollY: {
+    //         translateX: [
+    //             ["elInY", "elCenterY"],
+    //             [0, '500'],
+    //         ]
+    //     }
+    // })
+}
 
 function changeOwlMainHeight() {
     const owlMainItems = document.querySelectorAll(".owl-main__item");
@@ -33,7 +57,45 @@ $(document).ready(function () {
     });
 
     changeOwlMainHeight();
+
+    const allLang = document.querySelectorAll(".header__lang-all .text");
+    Array.from(allLang).forEach(element => {
+        element.addEventListener('click', () => {
+            document.querySelector('.header__lang-active-text').innerHTML = element.textContent;
+        });
+    });
+
+
+
+
 });
+
+var textWrapper = document.querySelector('.owl-main__heading');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+function animatedLetters() {
+
+    anime.timeline({
+            loop: false
+        })
+        .add({
+            targets: '.owl-main__heading .letter',
+            scale: [4, 1],
+            opacity: [0, 1],
+            translateZ: 0,
+            easing: "easeOutExpo",
+            duration: 950,
+            delay: (el, i) => 70 * i
+        }).add({
+            targets: '.ml2',
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
+}
+
+animatedLetters();
 
 let resizeTimer;
 $(window).on('resize', function (e) {
