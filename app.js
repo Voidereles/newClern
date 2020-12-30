e46f6b3e0054ca9bd7193efe5b0227283437e66f
Maciej <maciej.zelazo@protonmail.com>
2020-12-30 11:37:37 +0100

    lighthouse fixes

--- app.js
+++ app.js
@@ -51,7 +51,14 @@ $(document).ready(function () {
         }
     })
 
-
+    //Go through each carousel on the page
+    $('.owl-carousel').each(function () {
+        //Find each set of dots in this carousel
+        $(this).find('.owl-dot').each(function (index) {
+            //Add one to index so it starts from 1
+            $(this).attr('aria-label', index + 1);
+        });
+    });
 });
 
 var textWrapper = document.querySelector('.animated-heading');
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -24500,6 +24500,14 @@ $(document).ready(function () {
     } else {
       $(this).addClass('filled');
     }
+  }); //Go through each carousel on the page
+
+  $('.owl-carousel').each(function () {
+    //Find each set of dots in this carousel
+    $(this).find('.owl-dot').each(function (index) {
+      //Add one to index so it starts from 1
+      $(this).attr('aria-label', index + 1);
+    });
   });
 });
 var textWrapper = document.querySelector('.animated-heading');
